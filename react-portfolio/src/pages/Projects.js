e098bbd7a2c163e9e8af83d4257f83b4ff7d8035
oguerra9 <102640672+oguerra9@users.noreply.github.com>
2023-06-25 00:14:00 -0400

    added project cards and overlays

--- react-portfolio/src/pages/Projects.js
+++ react-portfolio/src/pages/Projects.js
@@ -1,10 +1,72 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
+import Card from 'react-bootstrap/Card';
+import DistanceCalcImage from '../images/distance-calculator-svg.svg';
+
 
 export default function Projects() {
+    const projectList = [
+        {
+            title: 'Distance Calculator',
+            repo_link: 'https://github.com/oguerra9/distance-calculator-SL23',
+            deploy_link: 'https://oguerra9.github.io/distance-calculator-SL23/',
+            skills: 'JavaScript, Google Maps API',
+            imgSrc: '../images/distance-calculator-image.png',
+            short_desc: 'Website used to calculate the distance between two geographic points and display the location of each.',
+            long_desc:''
+        },
+        {
+            title: 'Task Manager',
+            repo_link: 'https://github.com/oguerra9/task-tracker',
+            deploy_link: 'https://oguerra9.github.io/task-tracker/',
+            skills: 'React, Google Firebase',
+            imgSrc: '../images/task-manager-image.png',
+            short_desc: 'Website for users to create, edit, and delete tasks.',
+            long_desc: ''
+        },
+        {
+            title: 'Weather App',
+            repo_link: 'https://github.com/oguerra9/weatherApp',
+            deploy_link: 'https://oguerra9.github.io/weatherApp/',
+            skills: 'JavaScript',
+            imgSrc: '../images/weather-app-image.png',
+            short_desc: 'Website where users can enter the name of a location and see the current weather as well as a 5 day forecast.',
+            long_desc: ''
+        }
+        // {
+        //     title: '',
+        //     repo_link: '',
+        //     deploy_link: '',
+        //     skills: '',
+        //     imgSrc: '',
+        //     short_desc: '',
+        //     long_desc: ''
+        // }
+    ]
     return (
         <Container>
-            <div>this will be my projects page</div>
+            <h1>Past Projects</h1>
+            <Container id="projectDisplay" className="d-flex flex-wrap justify-content-center">
+                {projectList.map(project => (
+                    <Container id="projectItem" key={project.title} className="col-lg-5 m-4">
+                        <Card className="bg-dark text-white" id='projectCard'>
+                            <a href={project.deploy_link} target="_blank" rel="noreferrer" id="deployRef">
+                                {/* <Card.Img src={require('../images/task-manager-image.png')} alt="Card image" /> */}
+                                <Card.Img id="cardImg" src={project.imgSrc} alt="Card image" />
+                                <Card.ImgOverlay className="d-flex flex-column" style={{'backgroundColor':'purple', 'opacity':'0.75'}}>
+                                        <Card.Title>{project.title}</Card.Title>
+                                        <Card.Text>{project.short_desc}</Card.Text>
+                                        <Card.Text>{project.skills}</Card.Text>
+                                        <a href={project.repo_link} target="_blank" rel="noreferrer" id="repoIcon">
+                                            <img src={require("../images/github-mark-white.png")} id="githubIcon" alt="github icon"></img>
+                                        </a>
+                                    
+                                </Card.ImgOverlay>
+                            </a>
+                        </Card>
+                    </Container>
+                ))}
+            </Container>
         </Container>
     );
 }
